18862d64872bdc7be5fda8e1ad8e6115913cf6a7
legiorex <batyushev@gmail.com>
2019-03-12 13:29:29 +1000

    Начал проект

--- source/REST/config.js
+++ source/REST/config.js
@@ -1,5 +1,5 @@
 const ROOT_URL = 'https://lab.lectrum.io';
 const MAIN_URL = `${ROOT_URL}/hw/todo/api`;
-const TOKEN = '';
+const TOKEN = 'cGxNTMec853PUIu0';
 
 export { ROOT_URL, MAIN_URL, TOKEN };
